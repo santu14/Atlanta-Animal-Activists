d897ee19ec3b771f7acbdf359045054dca975f76
Eugenia <eugeniasol13@gmail.com>
2021-02-09 13:20:54 -0500

    navbar-base

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import React from "react";
+import Nav from "./components/Navbar";
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Nav>
+
+        </Nav>
         <p>
           ATLANTA ANIMAL ACTIVISTS
         </p>
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { AppBar, Button, Typography, IconButton, Toolbar, } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons';
+import "./Navbar.css";
+
+
+
+
+const Nav = () => {
+
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        News
+    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+
+
+}
+export default Nav;
