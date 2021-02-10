53f5a22f405d1bbc8360656acad7705f978a65fc
Eugenia <eugeniasol13@gmail.com>
2021-02-10 18:30:10 -0500

    grid&cssChanges

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,29 @@
 import './App.css';
 import React from 'react';
 import Nav from './components/Header';
-import { ThemeProvider, Grid } from '@material-ui/core';
+import { ThemeProvider, Grid, Paper } from '@material-ui/core';
 import theme from './components/Theme';
+import useStyles from './components/Styles';
 import AuthForm from './AuthForm'
 
 function App() {
+  const classes = useStyles();
   return (
     <ThemeProvider theme={theme} >
-      <Grid container direction='column' >
-        <Grid item ><Nav /></Grid>
-        <Grid item container className='App'>
+      <Grid container direction='column' spacing={3} className='base' >
+        <Grid item xs={12}><Nav /></Grid>
+        <Grid item xs={12} container className='App'>
 
           <Grid item xs={0} sm={2} />
+
           <Grid item xs={12} sm={8}>
-            <AuthForm />
+          <Paper className={classes.paper}>body test</Paper>
           </Grid>
+
           <Grid item xs={0} sm={2} />
 
+
+          {/* <AuthForm /> */}
 
         </Grid>
 
--- client/src/components/Styles.js
+++ client/src/components/Styles.js
@@ -1,9 +1,14 @@
 import { makeStyles } from '@material-ui/core';
-
+import { blueGrey } from '@material-ui/core/colors';
+import theme from './Theme';
 const useStyles = makeStyles (() => ({
     typographyStyles :{
         flex: 1
+    },
+    paper: {
+        backgroundColor: blueGrey[800],
+        padding: theme.spacing(2),   
     }
 }));
 
-export default useStyles
+export default useStyles;
