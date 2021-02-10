05be4b0823399194388054df4928bc01d73f71e4
Santiago Solana <santiago.sjs@gmail.com>
2021-02-09 20:52:32 -0500

    using cookies to store jwt

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,15 +2,19 @@ import axios from "axios";
 
 export default {
   // Gets all users
-  signUp: function(userData) {
-    return axios.post("/api/signup", userData).then( (data) => {
-        console.log(data);
-    });;
+  signUp: (userData) => {
+    return axios.post("/api/signup", userData).then((data) => {
+      console.log(data);
+    });
   },
-  signIn: function(userData) {
-    return axios.post("/api/signin", userData).then( (data) => {
-        console.log(data);
+  signIn: (userData) => {
+    return axios.post("/api/signin", userData).then((data) => {
+      console.log(data);
+    });
+  },
+  signOut: () => {
+    return axios.get("/api/signout").then((data) => {
+      console.log(data);
     });
   }
-
 };
