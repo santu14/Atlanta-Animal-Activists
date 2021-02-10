3de613d901a85d0bc6cf01f1cf9c912ff0b56d1d
Eugenia <eugeniasol13@gmail.com>
2021-02-09 22:03:24 -0500

    theme-styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
 
 import './App.css';
-import React from "react";
-import Nav from "./components/Navbar";
-
-
-
+import React from 'react';
+import Nav from './components/Navbar';
+import {ThemeProvider, Button, Tooltip, Typography, TextField} from '@material-ui/core';
+import theme from './components/Theme';
 import AuthForm from './AuthForm'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <ThemeProvider theme={theme}>
+    <div className='App'>
+      <header className='App-header'>
         <Nav>
 
         </Nav>
-        <p>
-          ATLANTA ANIMAL ACTIVISTS
-        </p>
+        <br/>
+        
+        <br/>
+
         <AuthForm/>
        
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { AppBar, Button, Typography, IconButton, Toolbar, } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons';
-import "./Navbar.css";
-
-
+import  Menu  from '@material-ui/icons/Menu';
 
 
 const Nav = () => {
@@ -12,11 +9,11 @@ const Nav = () => {
         <div>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu />
                     </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                    ATLANTA ANIMAL ACTIVISTS
+                    <Typography variant="h6" >
+                        ATLANTA ANIMAL ACTIVISTS
     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
