7117be0841a28a8350936e3aadae4e116cafb572
Eugenia <eugeniasol13@gmail.com>
2021-03-29 16:12:49 -0400

    events responsiveness

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Grid, Paper, Hidden, Button } from "@material-ui/core";
+import { Typography, Grid, Paper, Hidden } from "@material-ui/core";
 import useStyles from "../components/Styles";
 import Person from './assets/img/person.jpg'
 
@@ -8,35 +8,30 @@ const Events = () => {
     return (
         <div>
             <Grid item xs={12} container className={classes.eventCont} >
-                
-                <Grid item xs={4} className={classes.eventLine}/> 
-                <Grid item xs={4} /> 
-            
-                <Grid item xs={4}  container>
+
+                <Grid item xs={2} md={4} className={classes.eventLine} />
+                <Grid item xs={0} md={4} />
+
+                <Grid item xs={10} md={4} container>
                     <Paper className={classes.eventPaper} >
                         <Typography className={classes.eventTitle}>
                             Events and Updates
                         </Typography>
-                        <Hidden xsDown>
-                        <Typography item  className={classes.eventText}>
-                   We host many events and meet-ups throughout the year, they are regularly updated below but you can you also stay connected and up to date through our social media platforms. So if you want the lastes updates make sure to follow us on our Facebook or Twitter!
-      
+                        <Grid item className={classes.eventLine2} style={{ alignSelf: "left" }} />
+                        <Typography item className={classes.eventText}>
+                            We host many events and meet-ups throughout the year, they are regularly updated below but you can you also stay connected and up to date through our social media platforms. So if you want the lastes updates make sure to follow us on our Facebook or Twitter!
                     </Typography>
-                    
-                    </Hidden>
-                    
-
                     </Paper>
                 </Grid>
-                <Grid item  xs={12} container className={classes.personCont}>
-                
-                <Grid item>
-                <img className={classes.person} src={Person} alt='person pets cow'/>
+                <Grid item xs={12} container >
+
+                    <Grid item >
+                        <img className={classes.person} src={Person} alt='person pets cow' />
+                    </Grid>
+                    <Grid item className={classes.eventLine3} />
                 </Grid>
-                <Grid item className={classes.eventLine2}/>
-                </Grid>
-               
-                
+
+
             </Grid>
 
         </div>
